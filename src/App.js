a8b08cace095d2b83f1201372379576a87cade60
quimort <quimort97@gmail.com>
2023-11-19 11:41:54 +0100

    new app frame

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [imageUrl, setImageUrl] = useState('');
+  const [result, setResult] = useState('');
+
+  const analyzeImage = () => {
+    // Add your code for image analysis here
+    setResult(`Analyzing image: ${imageUrl}`);
+  };
+
+  const generateImage = () => {
+    // Add your code for image generation here
+    setResult(`Generating image based on: ${imageUrl}`);
+  };
+
+  return (
+    <div className="App">
+      <h1>Image Analyzer and Generator</h1>
+      <label htmlFor="imageURL">Enter Image URL:</label>
+      <input
+        type="text"
+        id="imageURL"
+        placeholder="https://example.com/image.jpg"
+        value={imageUrl}
+        onChange={(e) => setImageUrl(e.target.value)}
+      />
+      <br />
+      <button onClick={analyzeImage}>Analyze Image</button>
+      <button onClick={generateImage}>Generate Image</button>
+      <div id="result">{result}</div>
+    </div>
+  );
 }
 
 export default App;
